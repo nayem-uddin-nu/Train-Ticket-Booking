523b8a98eba0224b0b73382f91c43e47c5048e7f
Nayem Uddin <nayem@gigabit.agency>
2024-07-11 02:31:01 +0600

    handle unprocessed responsed from the server && prepared for show in view file

--- src/app/service/train.service.ts
+++ src/app/service/train.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ConfigService } from './config-service';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
    providedIn: 'root'
@@ -11,13 +12,30 @@ export class TrainService {
 
    constructor(private http: HttpClient) { }
 
-
    getAllStations() {
       return this.http.get(`${this.config.apiURL}/get-stations`);
    }
 
    getTickets(data: any) {
-      return this.http.post(`${this.config.apiURL}/search`, data);
+      return this.http.post(`${this.config.apiURL}/search`, data).pipe(
+         catchError(this.handleError)
+      );
+
+   }
+
+
+   private handleError(resp: HttpErrorResponse) {
+      console.log(resp.error);
+
+      let errorMessages = {};
+
+      if (resp.status === 422 && resp.error) {
+         errorMessages = resp.error;
+      } else {
+         errorMessages = { general: 'An error occurred. Please try again later.' };
+      }
+
+      return throwError(errorMessages);
    }
 
 }
